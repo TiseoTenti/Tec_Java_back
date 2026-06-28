c97d875e15b220375aba9fe6405411934cd178bf
Matias Benegas <mateobenegas@gmail.com>
2026-06-28 12:39:00 -0300

    first commit

---
+++ src/main/java/Tec_Java_app/Carrito/CarritoApplication.java
@@ -0,0 +1,13 @@
+package Tec_Java_app.Carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tec_Java_app/Carrito/CarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tec_Java_app.Carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
